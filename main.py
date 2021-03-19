4f815525fa81f88aa6004fa7a6e4ecd155152df2
Ottispottis <otto.s.heldt@gmail.com>
2021-03-19 13:26:08 +0200

    Added creator info

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+Yr.no web scraper
+
+Author: Otto Heldt
+
+Version 1.0
+
+Collects weather data from yr.no
+
+"""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
